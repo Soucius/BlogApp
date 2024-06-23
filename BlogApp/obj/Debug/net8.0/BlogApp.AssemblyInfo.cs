6099789e44633f3cedf0a3d0cc33c7ad1633067b
Soucius <soucius.shopping@gmal.com>
2024-06-24 00:11:02 +0300

    Created folder structure, prepare sql tables (code-first) and context...

--- BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
+++ BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b13a09bd20bde4967c2d2f9eeb58b3914f19a53")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
